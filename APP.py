59cc3a809e1605810d99a2451f65395a1aa130d1
AbdullayevKamron <124658627+AbdullayevKamron@users.noreply.github.com>
2023-02-17 11:07:06 +0500

    Foydalanuvchilar

---
+++ APP.py
@@ -0,0 +1,8 @@
+Foydalanuvchilar = []
+data = {
+    'Ism': input('Ismingiz:'),
+    'Yoshi': int(input('Yoshingiz:')),
+    'Uylanganmi': bool(input('Uylganimisiz:')),
+   "Qiziqishlari": list(input('Qiziqishlaringiz:').split())
+}
+print(data)
